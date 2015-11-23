865b33ca17b92ad168d82b6e13e6d1035bbf5ebe
Jessica Ro <jessro95@uw.edu>
2015-11-23 04:54:05 -0800

    final

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ myApp.controller('myCtrl', function($scope, $http){
 		data = $scope.infos = response;
 	});
 
+	$scope.getInfo = function(clicked) {
+		$('.info').empty();
+		var name = clicked.name;
+		var description = clicked.description;
+		var url = clicked.url;
+		var image = clicked.image;
+		$('.info').append('<a href="' + url + '"><img src ="'+ image +'" /></a>');
+		$('.info').append('<p> Project Title :' + name + '</p>');
+		$('.info').append('<p> Description : '+ description + '</p>');
+		$('.info').append('<p class="link"> <a href="' + url + '">link</a>  </p>');
+
+	}
+
 })
 	//Config route provider
 .config(function($stateProvider) {
@@ -18,15 +31,7 @@ myApp.controller('myCtrl', function($scope, $http){
 		controller:'WorkController',
 	})	
 
-	//more Info page
-	.state('moreInfo', {
-		url:'/moreInfo',
-		templateUrl: 'templates/moreInfo.html',
-		controller:function($scope, $stateParams) {
-			console.log($stateParams),
-			$scope.dat = $stateParams,
-		}
-	})
+
 
 	//about me page
 	.state('about', {
@@ -41,6 +46,14 @@ myApp.controller('myCtrl', function($scope, $http){
 		templateUrl: 'templates/contact.html',
 		controller:'ContactController'
 	})
+		//more Info page
+	.state('moreInfo', {
+		url:'/moreInfo/{data.name}',
+		templateUrl: 'templates/moreInfo.html',
+		controller:function($stateParams, $scope){
+			console.log($stateParams);
+		}
+	});
 
 })
 
@@ -49,7 +62,7 @@ myApp.controller('myCtrl', function($scope, $http){
 })
 
 .controller('AboutController', function($scope){
-	$scope.about = "I AM JESSICA RO AND I WANT ICE CREAM RIGHT NAO"
+	$scope.about = "I'm an Undergraduate Informatics student in University of Washington, and currently working as an instructor in Coding with Kids. I enjoy eating food from around the world."
 })
 
 .controller('ContactController', function($scope){
@@ -59,10 +72,5 @@ myApp.controller('myCtrl', function($scope, $http){
 })
 
 
-.controller('MoreInfoController', function($scope){
-		$scope.dat = data;
-		console.log($scope.dat)
-
-	})
 
 
